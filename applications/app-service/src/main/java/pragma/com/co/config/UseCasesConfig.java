72a15a658df7aa30e53da4f83d0973e02f832079
bolivero <bolivero@bancolombia.com.co>
2023-12-11 00:00:41 -0500

    Agrego arquitectura limpia

---
+++ applications/app-service/src/main/java/pragma/com/co/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package pragma.com.co.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "pragma.com.co.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
