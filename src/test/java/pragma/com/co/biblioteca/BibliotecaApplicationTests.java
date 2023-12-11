a1131f6f7075eb2bf69f20d1febb831c054521fe
bolivero <bolivero@bancolombia.com.co>
2023-12-10 23:18:27 -0500

    Creacion del proyecto

---
+++ src/test/java/pragma/com/co/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package pragma.com.co.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
